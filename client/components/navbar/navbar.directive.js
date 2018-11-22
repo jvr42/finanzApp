e43b55fea864ced0cb84806214f6853a95490767
Jesus Villarroel <jvr42@hotmail.com>
2018-11-22 16:10:19 -0300

    initial commit for application development

---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('finanzAppApp')
+  .directive('navbar', function () {
+    return {
+      templateUrl: 'components/navbar/navbar.html',
+      restrict: 'E',
+      controller: 'NavbarCtrl'
+    };
+  });
